52c688be12bef164f37da9b737bde9e47bcebb2d
tanyuzhou <tyz@gztyz.com>
2024-03-17 00:28:21 -0700

    update: course table component

--- src/components/course_table/CourseTable.vue
+++ src/components/course_table/CourseTable.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="course-table">
         <Year year="2021">
-            <Term termName="Fall 2021">
+            <Term termName="Fall">
                 <Course courseCode="COMP 1001" courseName="Introduction to Computing I" />
                 <Course courseCode="COMP 1002" courseName="Introduction to Computing II" />
                 <Course courseCode="COMP 1003" courseName="Introduction to Computing III" />
             </Term>
-            <Term termName="Winter 2022">
+            <Term termName="Winter">
                 <Course courseCode="COMP 2001" courseName="Data Structures and Algorithms" />
                 <Course courseCode="COMP 2002" courseName="Software Engineering" />
                 <Course courseCode="COMP 2003" courseName="Database Systems" />
             </Term>
         </Year>
         <Year year="2022">
-            <Term termName="Fall 2022">
+            <Term termName="Fall">
                 <Course courseCode="COMP 3001" courseName="Operating Systems" />
                 <Course courseCode="COMP 3002" courseName="Computer Networks" />
                 <Course courseCode="COMP 3003" courseName="Web Development" />
             </Term>
-            <Term termName="Winter 2023">
+            <Term termName="Winter">
                 <Course courseCode="COMP 4001" courseName="Artificial Intelligence" />
                 <Course courseCode="COMP 4002" courseName="Machine Learning" />
                 <Course courseCode="COMP 4003" courseName="Computer Vision" />
