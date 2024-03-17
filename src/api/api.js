f76eb532668dfed8823ddcfcc668e84606871595
tanyuzhou <tyz@gztyz.com>
2024-03-16 23:00:12 -0700

    fix: api error message display

--- src/api/api.js
+++ src/api/api.js
@@ -7,12 +7,12 @@ export const baseUrl = import.meta.env.VITE_API_BASE_URL
 export const defaultCallback = {
     succeed: (res) => {
         if(res.code != 0) {
-            message.error(res.msg)
+            message.error(res.message)
         }
         return res
     }, 
     failed: (res) => {
-        message.error(res.msg || "Failed to request")
+        message.error(res.message || "Failed to request")
         return res
     },
     network_error: (err) => {
